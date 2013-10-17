df21a5bf41ce718c9682f4d547ace8707a052dc1
Frank Xu <frank.w.xu@gmail.com>
2013-10-17 10:10:47 -0400

    add new to master first line

--- gittutorial/src/gittutorial/HelloGit.java
+++ gittutorial/src/gittutorial/HelloGit.java
@@ -3,6 +3,7 @@ package gittutorial;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		System.out.println("add something new");
 		System.out.println("Git is fun!!");
 		System.out.println("Git is fun!!");
 		System.out.println("Git is fun!!");
